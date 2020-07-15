b322b5046ab5afac7c6084eb388f9a78ccb9651c
Unknown <alexandresimardforbes@gmail.com>
2020-07-15 15:06:03 -0400

    Add some requests and define channels

---
+++ Web API/Channels.js
@@ -0,0 +1,20 @@
+const channels = [
+    {
+        id: 1,
+        name: 'Discovery',
+        genre: 'Science'
+    },
+    {
+        id: 2,
+        name: "ESPN",
+        genre: 'Sport'
+    },
+    {
+        id: 3,
+        name: 'Comedy central',
+        genre: 'Comedy'
+    }
+];
+
+module.exports = channels;
+
--- Web API/index.js
+++ Web API/index.js
@@ -1,6 +1,27 @@
-const express = require('express')
+const express = require('express');
+const path = require('path');
+const logger = require('./middleware/logger')
 
 const app = express();
+
+const channels = require('./Channels');
+const { filter } = require('./Channels');
+
 const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log('Server running'));
+
+//Init middleware
+app.use(logger);
+
+// Set static folder maybe for swagger
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Gets all channels
+app.get('/api/channels', (req, res) => res.json(channels));
+
+// Get single channel
+app.get('/api/channels/:id', (req, res) => {
+    res.json(channels.filter(channel => channel.id === parseInt(req.params.id)))
+});
 
 
