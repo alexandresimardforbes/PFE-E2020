7315fcaa003d9facec808808e3b3a855ec7bb4a3
Jonathan Ménard <Jonathann.menard@gmail.com>
2020-07-18 19:48:04 -0400

    first test with the web frame
    The application can use a web page, but it needs the interface to write url, etc..

--- Android application/app/src/main/java/com/example/noanandroidapplication/controller/ModeSelectionActivity.kt
+++ Android application/app/src/main/java/com/example/noanandroidapplication/controller/ModeSelectionActivity.kt
@@ -2,6 +2,7 @@ package com.example.noanandroidapplication.controller
 
 import android.app.Activity
 import android.os.Bundle
+import android.widget.Button
 import com.example.noanandroidapplication.R
 
 class ModeActivity : Activity() {
@@ -10,6 +11,14 @@ class ModeActivity : Activity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mode)
 
+        //we set listeners on the buttons
+        val buttonBack = findViewById<Button>(R.id.activity_mode_back_btn)
+        buttonBack?.setOnClickListener(){
+            finish()
+        }
+
     }
+
+
 
 }
