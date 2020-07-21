58044451e2fe900c55b85b1208e68ca2610a4e28
Charles Brunelle <charles.brunelle@logmein.com>
2020-07-21 18:43:58 -0400

    Add libraries Add Test activity

--- Android application/app/src/main/java/com/azuredev/android/MainActivity.java
+++ Android application/app/src/main/java/com/azuredev/android/MainActivity.java
@@ -2,6 +2,7 @@ package com.azuredev.android;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 
 import com.example.noanandroidapplication.R;
@@ -15,6 +16,9 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main_web);
 
         WebView myWebView = (WebView) findViewById(R.id.webView);
+        WebSettings webSettings = myWebView.getSettings();
+
+        webSettings.setJavaScriptEnabled(true);
 
         myWebView.loadUrl("https://www.google.com"); // todo Load local web page
     }
