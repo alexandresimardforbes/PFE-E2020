cc59f90ab59ab04ad5f55c98cbf88f3fef0e4173
Charles Brunelle <brunelle.ch@gmail.com>
2020-07-22 09:52:36 -0400

    Add JSInterface

--- Android application/app/src/main/java/com/azuredev/android/MainActivity.java
+++ Android application/app/src/main/java/com/azuredev/android/MainActivity.java
@@ -19,8 +19,11 @@ public class MainActivity extends Activity {
         WebSettings webSettings = myWebView.getSettings();
 
         webSettings.setJavaScriptEnabled(true);
+        myWebView.addJavascriptInterface(new JSInterface(this), "Android"); //You will access this via Android.method(args);
 
         myWebView.loadUrl("https://www.google.com"); // todo Load local web page
+
+//        myWebView.loadUrl("javascript: var result = window.Android.FetchJavaData(); window.DoStuff(result)");
     }
 
     @Override
