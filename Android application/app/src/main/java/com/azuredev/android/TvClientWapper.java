52e6b4eee564376d34cfdfa7399bd53d4d7a46af
Charles Brunelle <brunelle.ch@gmail.com>
2020-07-26 22:27:19 -0400

    Cleanup

--- Android application/app/src/main/java/com/azuredev/android/TvClientWapper.java
+++ Android application/app/src/main/java/com/azuredev/android/TvClientWapper.java
@@ -1,41 +1,21 @@
 package com.azuredev.android;
 
-import android.app.Activity;
-import android.app.ActivityManager;
-import android.content.BroadcastReceiver;
 import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
-import android.content.pm.PackageManager;
-import android.os.Bundle;
-import android.preference.PreferenceManager;
-import android.util.Log;
-import android.view.Window;
-import android.view.WindowManager;
 import android.widget.VideoView;
 
 import com.amlogic.tvclient.TVClient;
 import com.amlogic.tvutil.TVConst;
 import com.amlogic.tvutil.TVMessage;
 
-import java.util.List;
-
 public class TvClientWapper extends TVClient {
     private VideoView videoView;
-    private Context mContext;
     private boolean connected;
-    private boolean externalVideoView;
-    private boolean delay_setinput_source = false;
-    private static int dtvactivity_actived_num = 0;
 
     @Override
     public void onConnected() {
         connected = true;
         updateVideoWindow();
-        if (delay_setinput_source) {
-            delay_setinput_source = false;
-            setInputSource(TVConst.SourceInput.SOURCE_DTV);
-        }
+        setInputSource(TVConst.SourceInput.SOURCE_DTV);
         openVideo();
         this.resume();
         playProgram(4);
@@ -56,12 +36,10 @@ public class TvClientWapper extends TVClient {
     }
 
     public void updateVideoWindow() {
-        if (this.videoView == null) {
+        if (this.videoView == null || !this.connected) {
             return;
         }
-        if (!this.connected) {
-            return;
-        }
+
         final int[] loc = new int[2];
         this.videoView.getLocationOnScreen(loc);
 
@@ -77,99 +55,15 @@ public class TvClientWapper extends TVClient {
     }
 
     private void solveMessage(final TVMessage msg) {
-//        switch (msg.getType()) {
-//            case 15: {
-//                this.onProgramStart(msg.getProgramID());
-//                this.updateSmallWindow();
-//                break;
-//            }
-//            case 16: {
-//                this.onProgramStop(msg.getProgramID());
-//                break;
-//            }
-//            case 45: {
-//                this.onPlaybackStart();
-//                break;
-//            }
-//            case 46: {
-//                this.onPlaybackStop();
-//                break;
-//            }
-//            case 9: {
-//                try {
-//                    this.onConfigChanged(msg.getConfigName(), msg.getConfigValue());
-//                }
-//                catch (Exception e) {
-//                    Log.e("TVActivity", "error in onConfigChanged");
-//                }
-//                break;
-//            }
-//            case 53: {
-//                this.finish();
-//                break;
-//            }
-//        }
     }
 
     public void openVideo() {
-        this.externalVideoView = true;
         this.videoView.getHolder().setFormat(258);
         this.updateVideoWindow();
-        this.startPhoneSerVer();
-    }
-
-    public void startPhoneSerVer() {
-        if (getBooleanConfig("tv:dtv:phone:watch")) {
-//            if (!this.isServiceRunning("com.amlogic.tvbox.PhoneServer")) {
-//                final Intent intent2 = new Intent("com.amlogic.tvbox.PhoneServer");
-//                intent2.setFlags(268435456);
-//                this.startService(intent2);
-//            }
-//            this.bindService(new Intent("com.amlogic.tvbox.PhoneServer"), this.connection, 1);
-        }
     }
 
     public void onCreate(final Context context, final VideoView videoView) {
         this.videoView = videoView;
-        this.mContext = context;
-        connect(this.mContext);
-
-        DTVActivity_ActivedStateManage(true);
-    }
-
-    private void DTVActivity_ActivedStateManage(boolean actived) {
-//        Log.d(TAG, "DTVActivity_ActivedStateManage actived " + actived + " dtvactivity_actived_num " + dtvactivity_actived_num);
-        if (actived) {
-            if (dtvactivity_actived_num == 0) {
-                /* stop music play when dtv actived */
-//                Intent stopMusicIntent = new Intent();
-//                stopMusicIntent.setAction("com.android.music.musicservicecommand.pause");
-//                stopMusicIntent.putExtra("command", "stop");
-//                sendBroadcast(stopMusicIntent);
-
-//                Intent exitMusicIntent = new Intent();
-//                exitMusicIntent.setAction("com.android.music.musicservicecommand.pause");
-//                exitMusicIntent.putExtra("command", "exit");
-//                sendBroadcast(exitMusicIntent);
-
-                /* set vpath */
-                if (connected) {
-                    setInputSource(TVConst.SourceInput.SOURCE_DTV);
-                } else {
-                    delay_setinput_source = true;
-                }
-
-//                Log.d(TAG, "DTVActivity_ActivedStateManage actived stop music set vpath");
-            }
-
-            dtvactivity_actived_num++;
-        } else {
-            dtvactivity_actived_num--;
-
-            if (dtvactivity_actived_num == 0) {
-                /* reset vpath, this is borrow SOURCE_ATV parameter */
-                setInputSource(TVConst.SourceInput.SOURCE_ATV);
-            }
-        }
+        connect(context);
     }
 }
