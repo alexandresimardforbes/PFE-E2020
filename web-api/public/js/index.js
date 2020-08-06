c9592a216003f917e6c13345944dbade11459386
Unknown <alexandresimardforbes@gmail.com>
2020-08-06 15:34:57 -0400

    Started menu

--- web-api/public/js/index.js
+++ web-api/public/js/index.js
@@ -7,12 +7,17 @@ $(document).ready(function(){
         $("#uid").text("api not loaded");
     }
   });
+  
+$.getScript('https://luke-chang.github.io/js-spatial-navigation/spatial_navigation.js', function() {
+  $('a, .focusable')
+    .SpatialNavigation()
+    .focus()
+    .blur()
+    .first()
+    .focus();
+});
 
-  $.getScript('https://luke-chang.github.io/js-spatial-navigation/spatial_navigation.js', function() {
-    $('a, .focusable')
-      .SpatialNavigation()
-      .focus()
-      .blur()
-      .first()
-      .focus();
-  });
+function toggleVisibility() {
+  console.log("buttonClick");
+  $('.background').toggleClass("invisible");
+}
